1128b601bdd679bebe4e76a73aa86bc5a8b86d85
Daniel Gonzalez <daniel.gonzalez.02@gmail.com>
2023-05-23 22:56:49 +0200

    feat: create react router

--- src/pages/router/Router.jsx
+++ src/pages/router/Router.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from '../home/Home';
 
+/**
+ * React component given the router DOM of the application
+ * @returns {React.ReactElement} Router
+ */
 const Router = () => {
   return (
-    <div>
-      
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
