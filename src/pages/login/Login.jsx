fc4012a770fc8c030010a17796e84e5b392cf1c5
Daniel Gonzalez <daniel.gonzalez.02@gmail.com>
2023-05-24 17:00:51 +0200

    feat: create login page

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
+import Navigation from '../../components/navigation/Navigation';
+import Footer from '../../components/footer/Footer';
+import LoginForm from '../../components/loginForm/LoginForm';
 
 /**
  * React component given the login page of the application
  * @returns {React.ReactElement} Login
  */
 const Login = () => {
-    return (
-        <div>
-            Login Page
-        </div>
-    );
+  return (
+    <div className="body-wrapper">
+      <Navigation />
+      <main className="main bg-dark">
+        <LoginForm />
+      </main>
+      <Footer />
+    </div>
+  );
 };
 
 export default Login;
